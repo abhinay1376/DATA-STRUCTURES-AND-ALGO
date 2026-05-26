c6d4ef6462ff947203506a790a4acf3225d83c1f
abhinay1376 <abhinay1376@gmail.com>
2026-05-26 21:43:14 +0530

    prime number

---
+++ Basic problems/prime_number.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class prime_number {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is prime");
+        } else {
+            System.out.println(num + " is not prime");
+        }
+    }
+}   
