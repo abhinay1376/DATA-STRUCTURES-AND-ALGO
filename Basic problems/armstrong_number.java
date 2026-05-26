5f67349e4e824d50f7803f8197c9f1a1b96ab202
abhinay1376 <abhinay1376@gmail.com>
2026-05-26 21:49:27 +0530

    armstrong number

---
+++ Basic problems/armstrong_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class armstrong_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int originalNum = num;
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+        if (sum == originalNum) {
+            System.out.println(originalNum + " is an Armstrong number");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong number");
+        }
+    }
+}
