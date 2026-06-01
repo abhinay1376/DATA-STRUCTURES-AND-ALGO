1dd97d16dd1dc0a720ca7b07c8c0ee2a045ba73d
abhinay1376 <abhinay1376@gmail.com>
2026-06-01 19:20:03 +0530

    to print all the subarrays of the Array

---
+++ Arrays/Easy/Subarray.java
@@ -0,0 +1,18 @@
+class Subarray{
+    public static void main(String args[]){
+        int[] arr={1,2,3,4,5,6,7};
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                     for(int k=i;k<j;k++){
+                        System.out.print(arr[k]);
+                     }
+                 System.out.print(" ");
+
+            }
+          System.out.println();
+
+        }
+
+    }
+}
