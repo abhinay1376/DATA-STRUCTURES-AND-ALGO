699ad21a449c7469b7fa1cbeefd7f6c0fe9be7f9
abhinay1376 <abhinay1376@gmail.com>
2026-06-02 20:50:19 +0530

    update subarray implementations

--- Arrays/Easy/Subarray.java
+++ Arrays/Easy/Subarray.java
@@ -2,17 +2,17 @@ class Subarray{
     public static void main(String args[]){
         int[] arr={1,2,3,4,5,6,7};
         int n=arr.length;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                     for(int k=i;k<j;k++){
-                        System.out.print(arr[k]);
-                     }
-                 System.out.print(" ");
-
-            }
-          System.out.println();
-
+        int sum=0;
+       for(int i=0;i<n;i++){
+    for(int j=i;j<n;j++){
+        for(int k=i;k<=j;k++){
+            System.out.print(arr[k]);
+        
         }
 
+        System.out.print(" ");
+    }
+    System.out.println();
+}
     }
 }
