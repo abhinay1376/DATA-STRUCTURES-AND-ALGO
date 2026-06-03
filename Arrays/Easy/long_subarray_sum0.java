3166c0c31efbc98bc59fdfd1c1c688e6bbef7bcb
abhinay1376 <abhinay1376@gmail.com>
2026-06-03 22:59:03 +0530

    long_subarray_sum0

---
+++ Arrays/Easy/long_subarray_sum0.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class long_subarray_sum0 {
+    public static void main(String[] args) {
+        int[] arr = { 10, 5, 2, 7, 1, 9 };
+
+        System.out.println(longestSubarray(arr));
+    }
+
+    public static int longestSubarray(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        int prefix_sum = 0;
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            prefix_sum = prefix_sum + arr[i];
+            if (prefix_sum == k) {
+                max = i + 1;
+            }
+            int remaining = prefix_sum - k;
+
+            if (map.containsKey(remaining)) {
+                int length = i - map.get(remaining);
+                max = Math.max(length, max);
+            }
+            if (!(map.containsKey(prefix_sum))) {
+                map.put(prefix_sum, i);
+            }
+
+        }
+        return max;
+    }
+}
