ede3b4d2040ba1eb534fafbbdf243cf9372c5cd7
abhinay1376 <abhinay1376@gmail.com>
2026-06-03 20:53:17 +0530

    optimal approach using the HashMap and prefix

--- Arrays/Easy/longest_subarray.java
+++ Arrays/Easy/longest_subarray.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class longest_subarray {
     public static void main(String[] args) {
         int[] arr = { 10, 5, 2, 7, 1, 9 };
@@ -7,21 +9,25 @@ public class longest_subarray {
     }
 
     public static int longestSubarray(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
         int max = 0;
+        int prefix_sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                int sum = 0;
-                for (int m = i; m <= j; m++) {
-                    sum = sum + arr[m];
-                }
-                if (sum == k) {
-                    int length = j - i + 1;
-                    max = Math.max(length, max);
-                }
-
+            prefix_sum += arr[i];
+            if (prefix_sum == k) {
+                max = i + 1;
             }
+            int remaining = prefix_sum - k;
+
+            if (map.containsKey(remaining)) {
+                int length = i - map.get(remaining);
+                max = Math.max(length, max);
+            }
+            if (!map.containsKey(prefix_sum)) {
+                map.put(prefix_sum, i);
+            }
+
         }
         return max;
     }
-
 }
