bf56b28708451199a915fcfbd2bf5c24d6c3e734
Abhinay Soleghe <abhinay1376@gmail.com>
2026-06-04 21:40:30 +0530

    Time: 1 ms (99.88%) | Memory: 55.7 MB (30.74%) - LeetSync

--- Leetcode_Problems/169-majority-element/majority-element.java
+++ Leetcode_Problems/169-majority-element/majority-element.java
@@ -1,6 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int majority_element=nums[0];
+        int count =0;
+        for(int i=0;i<nums.length;i++){
+             if(count==0){
+                majority_element=nums[i];
+                count++;
+            }
+            else if(majority_element==nums[i]){
+                count++;
+
+            }
+            else {
+                count--;
+            }
+
+            
+        }
+        return majority_element;
     }
 }
