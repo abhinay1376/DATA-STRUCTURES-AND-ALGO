c9fb015c1c77f6b477471e9814ea5c203f934ed6
Abhinay Soleghe <abhinay1376@gmail.com>
2026-06-04 21:50:01 +0530

    Time: 1 ms (99.95%) | Memory: 77 MB (80.86%) - LeetSync

--- Leetcode_Problems/53-maximum-subarray/maximum-subarray.java
+++ Leetcode_Problems/53-maximum-subarray/maximum-subarray.java
@@ -6,7 +6,7 @@ class Solution {
             curr_sum+=nums[i];
             max_sum=Math.max(curr_sum,max_sum);
             if(curr_sum<0)
-               curr_sum=0;
+            curr_sum=0;
         }
         return max_sum;
     }
