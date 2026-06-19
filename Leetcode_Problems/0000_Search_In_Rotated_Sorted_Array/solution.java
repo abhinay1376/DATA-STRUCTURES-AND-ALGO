524ee19a7ca728cc71ff1748166df287dc7ba066
Abhinay Soleghe <abhinay1376@gmail.com>
2026-06-19 20:33:13 +0530

    feat(leetcode): solve Search in Rotated Sorted Array

--- Leetcode_Problems/0000_Search_In_Rotated_Sorted_Array/solution.java
+++ Leetcode_Problems/0000_Search_In_Rotated_Sorted_Array/solution.java
@@ -1,10 +1,24 @@
 1class Solution {
 2    public int search(int[] nums, int target) {
-3        for(int i=0;i<nums.length;i++){
-4            if(nums[i]==target)
-5            return i;
-6           
-7        }
-8        return -1;
-9    }
-10}
+3          int left=0;
+4          int n=nums.length;
+5          int right=n-1;
+6          while(left<=right){
+7            int mid=(right+left)/2;
+8            if(nums[mid]==target) return mid;
+9            if(nums[left]<=nums[mid]){
+10                if(nums[left]<=target&&target<=nums[mid])
+11                right=mid-1;
+12                else 
+13                left=mid+1;
+14            }
+15            else{
+16                if(nums[mid]<=target&&target<=nums[right])
+17                left=mid+1;
+18                else
+19                right=mid-1;
+20            }
+21          }
+22          return -1;
+23    }
+24}
