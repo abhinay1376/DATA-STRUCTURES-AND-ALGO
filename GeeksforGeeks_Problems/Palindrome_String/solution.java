2d290043f32ffbed044e880f0bee1d0b95596075
Abhinay Soleghe <abhinay1376@gmail.com>
2026-06-23 21:41:02 +0530

    feat(gfg): solve Palindrome String

---
+++ GeeksforGeeks_Problems/Palindrome_String/solution.java
@@ -0,0 +1,8 @@
+class Solution:
+	def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return 0
+            left, right = left+1, right-1
+        return 1
