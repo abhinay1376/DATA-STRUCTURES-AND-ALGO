0ac08446246e1bd012fd1b04cafbc37d22a0291e
Abhinay Soleghe <abhinay1376@gmail.com>
2026-06-27 18:23:30 +0530

    feat(leetcode): solve Kth Missing Positive Number

---
+++ Leetcode_Problems/0000_Kth_Missing_Positive_Number/solution.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public int findKthPositive(int[] arr, int k) {
+3        for(int i=0;i<arr.length;i++){
+4            if(arr[i]<=k){
+5                k++;
+6            }
+7            else 
+8               break;
+9        }
+10        return k;
+11    }
+12}
