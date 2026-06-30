6774ed13f8de81196d1b2d20c9a9d4af407b138f
Abhinay Soleghe <abhinay1376@gmail.com>
2026-06-30 20:03:50 +0530

    feat(leetcode): solve Pascal's Triangle

--- Leetcode_Problems/0000_Pascals_Triangle/solution.java
+++ Leetcode_Problems/0000_Pascals_Triangle/solution.java
@@ -1,18 +1,19 @@
 1class Solution {
 2    public List<List<Integer>> generate(int numRows) {
-3        List<List<Integer>> ans = new ArrayList<>();
+3        List<List<Integer>> ans=new ArrayList<>();
 4        for(int i=0;i<numRows;i++){
-5            List<Integer> temp = new ArrayList<>();
+5                   List<Integer> temp=new ArrayList<>();
 6            for(int j=0;j<=i;j++){
-7                if(j == 0 || j == i) temp.add(1);
-8                else{
-9                    int val = ans.get(i-1).get(j-1)+ ans.get(i-1).get(j);
-10                    temp.add(val);
-11                }
-12            }
-13            ans.add(new ArrayList<>(temp));
-14        }
-15        return ans;
-16    }
-17}
-18
+7                if(j==0||j==i) 
+8                temp.add(1);
+9                else{
+10                   int  val=ans.get(i-1).get(j-1)+ans.get(i-1).get(j);
+11                    temp.add(val);
+12                }
+13            }
+14            ans.add(new ArrayList(temp));
+15        }
+16        return ans;
+17        
+18    }
+19}
