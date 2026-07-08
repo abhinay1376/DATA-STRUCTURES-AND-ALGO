5c155835b24f8381689c3911cfbc1be8128f917d
Abhinay Soleghe <abhinay1376@gmail.com>
2026-07-08 21:13:49 +0530

    feat(leetcode): solve Remove Duplicates from Sorted Array

--- Leetcode_Problems/0000_Remove_Duplicates_From_Sorted_Array/solution.java
+++ Leetcode_Problems/0000_Remove_Duplicates_From_Sorted_Array/solution.java
@@ -1,12 +1,15 @@
 1class Solution {
 2    public int removeDuplicates(int[] nums) {
-3        int i=0;
-4        for(int j=1;j<nums.length;j++){
-5            if(nums[i]!=nums[j]){
-6                i++;
-7                nums[i]=nums[j];
-8            }
-9        }
-10    return i+1;
-11    }
-12}
+3        int left=0;
+4        int right=1;
+5        while(right<nums.length){
+6            if(nums[left]!=nums[right]){
+7                left++;
+8                nums[left]=nums[right];
+9            }
+10            right++;
+11        }
+12        return left+1;
+13        
+14    }
+15}
