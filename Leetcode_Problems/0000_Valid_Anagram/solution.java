b1f5d026afef92165e7d15a039df11cb8143ed42
Abhinay Soleghe <abhinay1376@gmail.com>
2026-07-14 20:42:48 +0530

    feat(leetcode): solve Valid Anagram

--- Leetcode_Problems/0000_Valid_Anagram/solution.java
+++ Leetcode_Problems/0000_Valid_Anagram/solution.java
@@ -1,14 +1,19 @@
 1class Solution {
 2    public boolean isAnagram(String s, String t) {
-3       HashMap<Character,Integer> map=new HashMap<>(); 
-4              HashMap<Character,Integer> map1=new HashMap<>(); 
-5              for(int i=0;i<s.length();i++){
-6                  map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-7              }
-8 for(int i=0;i<t.length();i++){
-9                  map1.put(t.charAt(i),map1.getOrDefault(t.charAt(i),0)+1);
-10              }
-11              return map.equals(map1);
-12
-13    }
-14}
+3      
+4        HashMap<Character,Integer> map=new HashMap<>();
+5        HashMap<Character,Integer> map1=new HashMap<>();
+6        for(int i=0;i<s.length();i++)
+7{
+8    map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+9}
+10        for(int i=0;i<t.length();i++)
+11{
+12    map1.put(t.charAt(i),map1.getOrDefault(t.charAt(i),0)+1);
+13}
+14
+15
+16
+17        return map.equals(map1);
+18    }
+19}
