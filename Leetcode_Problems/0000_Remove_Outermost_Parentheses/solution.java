1d4793830d04b37620ba56bfc4d4ec671ce0673e
Abhinay Soleghe <abhinay1376@gmail.com>
2026-07-15 21:58:43 +0530

    feat(leetcode): solve Remove Outermost Parentheses

--- Leetcode_Problems/0000_Remove_Outermost_Parentheses/solution.java
+++ Leetcode_Problems/0000_Remove_Outermost_Parentheses/solution.java
@@ -1,9 +1,15 @@
-public String removeOuterParentheses(String S) {
-        StringBuilder s = new StringBuilder();
-        int opened = 0;
-        for (char c : S.toCharArray()) {
-            if (c == '(' && opened++ > 0) s.append(c);
-            if (c == ')' && opened-- > 1) s.append(c);
-        }
-        return s.toString();
-    }
+1class Solution {
+2public String removeOuterParentheses(String S) {
+3    StringBuilder s = new StringBuilder();
+4    int opened = 0;
+5    for (char c : S.toCharArray()) {
+6        if (c == '(' && opened++ > 0) {
+7            s.append(c);
+8        }
+9        if (c == ')' && opened-- > 1) {
+10            s.append(c);
+11        }
+12    }
+13    return s.toString();
+14}
+15}
