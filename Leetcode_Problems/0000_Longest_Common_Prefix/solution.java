dfa5c026e14598ae0f195e05df1ee864329deec3
Abhinay Soleghe <abhinay1376@gmail.com>
2026-07-17 20:57:35 +0530

    feat(leetcode): solve Longest Common Prefix

--- Leetcode_Problems/0000_Longest_Common_Prefix/solution.java
+++ Leetcode_Problems/0000_Longest_Common_Prefix/solution.java
@@ -1,12 +1,15 @@
 1class Solution {
 2    public String longestCommonPrefix(String[] strs) {
 3        Arrays.sort(strs);
-4        String s=new String(strs[0]);
-5        String t=new String(strs[strs.length-1]);
-6        for(int i=0;i<t.length();i++){
-7            if(s.charAt(i)!=t.charAt(i))
-8            return s.substring(0,i);
-9        }
-10        return "";
-11    }
-12}
+4        String s=strs[0];
+5        int count=0;
+6        String t=strs[strs.length-1];
+7        for(int i=0;i<s.length();i++){
+8            if(s.charAt(i)==t.charAt(i))
+9            count++;
+10            else 
+11            break;
+12        }
+13        return s.substring(0,count);
+14    }
+15}
